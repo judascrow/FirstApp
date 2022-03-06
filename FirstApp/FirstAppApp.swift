bc4650a0d8c69e0a98d0602f3ce2931c970e2697
judascrow <judascrow1@gmail.com>
2022-03-07 00:29:30 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by technoapp on 7/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
